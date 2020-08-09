5db37c7727a1c10761f672dad2b5e200ef92e853
bardur <zygneapps@gmail.com>
2020-08-09 11:30:10 -0500

    oui for HP inc.

--- bluetooth/src/main/java/com/zygne/bluetooth/domain/uitls/Vendors.kt
+++ bluetooth/src/main/java/com/zygne/bluetooth/domain/uitls/Vendors.kt
@@ -10,6 +10,11 @@ object Vendors {
         "54L:81:2D" to "PAX Computer Technology(Shenzhen) Ltd.",
         "00:17:6F" to "PAX Computer Technology(Shenzhen) Ltd.",
         "B0:F1:EC" to "AMPAK Technology, Inc.",
-        "0C:B5:27" to "HUAWEI TECHNOLOGIES CO.,LTD"
+        "0C:B5:27" to "HUAWEI TECHNOLOGIES CO.,LTD",
+        "E8:D8:D1" to "HP Inc.",
+        "38:22:E2" to "HP Inc.",
+        "B0:5C:DA" to "HP Inc.",
+        "04:0E:3C" to "HP Inc.",
+        "00:68:EB" to "HP Inc."
     )
 }
